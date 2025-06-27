c4326d1dfdcaa4f066ae29a4e492898a09a9f0da
Habib Ullah <habib@siddiqueice.com>
2025-06-27 16:53:33 +0500

    debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
